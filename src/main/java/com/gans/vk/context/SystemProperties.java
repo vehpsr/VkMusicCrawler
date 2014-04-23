41d44d01fba5118c2c4ce6d9d362f3e57c39b779
gans <vehpsrr@i.ua>
2014-04-23 22:20:45 +0300

    set up system properties

---
+++ src/main/java/com/gans/vk/context/SystemProperties.java
@@ -0,0 +1,52 @@
+package com.gans.vk.context;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.MessageFormat;
+import java.util.Properties;
+
+public class SystemProperties {
+
+	private final static String PROPERTY_FILE_NAME = "app.properties";
+	private static Properties properties = loadProperties();
+	
+	public enum Property {
+		VK_PASS("vk.pass"),
+		VK_LOGIN("vk.login"),
+		VK_ID("vk.id");
+		
+		private String key;
+		
+		Property(String key) {
+			this.key = key;
+		}
+	}
+	
+	private static Properties loadProperties() {
+		Properties props = new Properties();
+		InputStream is = ClassLoader.getSystemResourceAsStream(PROPERTY_FILE_NAME);
+		if (is == null) {
+			throw new AssertionError(MessageFormat.format("Fail to read property file {0}: does not exists or invalid", PROPERTY_FILE_NAME));
+		}
+    	try {
+			props.load(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return props;
+	}
+	
+	public static String get(Property prop) {
+		return properties.getProperty(prop.key);
+	}
+
+	public static String get(Property prop, String defaultValue) {
+		return properties.getProperty(prop.key, defaultValue);
+	}
+}
