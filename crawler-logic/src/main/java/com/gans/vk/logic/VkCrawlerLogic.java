753cbb66d7ac6e3728d07a8c195a73929340c2fd
gans <vehpsrr@i.ua>
2014-05-20 21:59:13 +0300

    add google collections to project

--- crawler-logic/src/main/java/com/gans/vk/logic/VkCrawlerLogic.java
+++ crawler-logic/src/main/java/com/gans/vk/logic/VkCrawlerLogic.java
@@ -1,15 +1,19 @@
 package com.gans.vk.logic;
 
-import java.util.List;
+import java.util.*;
+import java.util.Map.Entry;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import com.gans.vk.context.SystemProperties;
 import com.gans.vk.data.AudioLibrary;
+import com.gans.vk.logic.data.Metric;
 import com.gans.vk.logic.processor.AudioProcessor;
 import com.gans.vk.logic.service.LogicService;
 import com.gans.vk.logic.service.impl.LogicServiceImpl;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 
 public class VkCrawlerLogic {
 
@@ -29,5 +33,14 @@ public class VkCrawlerLogic {
     private void start() {
         List<AudioProcessor> processors = _logicService.getProcessors();
         List<AudioLibrary> allAudio = _logicService.getAllAudio();
+
+        Multimap<String, Entry<AudioProcessor, Metric>> metrics = ArrayListMultimap.create();
+        for (AudioLibrary lib : allAudio) {
+            for (AudioProcessor processor : processors) {
+                Entry<String, Metric> entry = processor.evaluate(lib);
+                metrics.put(entry.getKey(), new AbstractMap.SimpleEntry<AudioProcessor, Metric>(processor, entry.getValue()));
+            }
+        }
+
     }
 }
